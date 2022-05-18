342f1bd07bd2085dc0e5021d1fcbe30b8bf4bbc9
Francis J <fajoaq@gmail.com>
2022-05-18 19:29:06 -0400

    hero and cta styling

--- src/components/hero/CallToAction.jsx
+++ src/components/hero/CallToAction.jsx
@@ -2,7 +2,14 @@ import styles from "./hero.module.css";
 
 const CallToAction = (props) => (
   <div class={styles.cta_container} {...props}>
-    Call To Action
+    <p>Looking to Shop for Style?</p>
+    <h1>Furnish the Home of Your Dreams.</h1>
+    <p>
+      Shop modern furniture selections that add beauty and elegance to any home.
+    </p>
+    <div class={styles.cta_button}>
+      <a>Shop Now</a>
+    </div>
   </div>
 );
 
