86e6ad56b23dfa0cf1a38e03b0ed5470681ba79f
Francis J <fajoaq@gmail.com>
2022-05-23 19:06:25 -0400

    styled full rooms section, added bg images

--- src/components/full-rooms/FullRoomSection.jsx
+++ src/components/full-rooms/FullRoomSection.jsx
@@ -2,8 +2,35 @@ import styles from "./room-section.module.scss";
 
 const FullRoomSection = (props) => (
   <div class={styles.container} {...props}>
-    <div class={styles.room_section}>Living Room</div>
-    <div class={styles.room_section}>Bedroom</div>
+    <div class={styles.cta_container} {...props}>
+      <h2>Bedrooms</h2>
+      <p>From 149.99</p>
+      <div class={styles.cta_button}>
+        <a>Shop Now</a>
+      </div>
+
+      <div class={styles.bg}>
+        <img
+          src="/src/assets/bg/bedroom-full-room.jpg"
+          alt="Gallery image one."
+        />
+      </div>
+    </div>
+
+    <div class={styles.cta_container} {...props}>
+      <h2>Living Rooms</h2>
+      <p>From 149.99</p>
+      <div class={styles.cta_button}>
+        <a>Shop Now</a>
+      </div>
+
+      <div class={styles.bg}>
+        <img
+          src="/src/assets/bg/living-room-full-room.jpg"
+          alt="Gallery image two."
+        />
+      </div>
+    </div>
   </div>
 );
 
