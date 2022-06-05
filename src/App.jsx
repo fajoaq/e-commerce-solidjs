5997cab5ac4a6add786ff225f2cdeaf6056b5511
Francis J <fajoaq@gmail.com>
2022-06-05 05:46:02 -0400

    set device type after component mount

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { appState, setAppState } from "./store/app.store";
 import { HomePageLayout } from "./layout/homepage/homepage.layout";
 import { CONSTANTS } from "./utils/constants";
 import { MainNavigation } from "./components/navigation/MainNavigation";
+import { onMount } from "solid-js";
 
 function App() {
   // set device type
-  if (window.innerWidth > CONSTANTS.mobileSize)
-    setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.desktop });
-  else setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.mobile });
+  onMount(() => {
+    if (window.innerWidth > CONSTANTS.mobileSize)
+      setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.desktop });
+    else setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.mobile });
+  });
 
   return (
     <>
