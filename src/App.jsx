0c9daea0754040b18d29e3160a276143f4a39853
Francis J <fajoaq@gmail.com>
2022-06-05 09:02:00 -0400

    fixed device type being set late

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { appState, setAppState } from "./store/app.store";
 import { HomePageLayout } from "./layout/homepage/homepage.layout";
 import { CONSTANTS } from "./utils/constants";
 import { MainNavigation } from "./components/navigation/MainNavigation";
-import { onMount } from "solid-js";
 
 function App() {
   // set device type
-  onMount(() => {
-    if (window.innerWidth > CONSTANTS.mobileSize)
-      setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.desktop });
-    else setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.mobile });
-  });
+  if (window.innerWidth > CONSTANTS.mobileSize)
+    setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.desktop });
+  else setAppState({ ...appState, deviceType: CONSTANTS.deviceTypes.mobile });
 
   return (
     <>
